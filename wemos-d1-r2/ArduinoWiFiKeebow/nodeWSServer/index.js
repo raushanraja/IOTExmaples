31a627831606bc1def551e6d6a1f0116d7108eaf
rausahanraja <vraushanraja@gmail.com>
2021-02-26 21:49:22 +0530

    add cycle through output sinks

--- wemos-d1-r2/ArduinoWiFiKeebow/nodeWSServer/index.js
+++ wemos-d1-r2/ArduinoWiFiKeebow/nodeWSServer/index.js
@@ -1,38 +1,62 @@
-const socket = require('socket.io')();
-const { exec } = require('child_process');
+const socket = require("socket.io")();
+const util = require("util");
+const asyncExec = util.promisify(require("child_process").exec);
+const { exec } = require("child_process");
 
+let currentSelectedSink;
 
- 
+socket.on("connection", (client) => {
+  console.log("Connected", " client id:", client.id);
+  client.emit("init", { message: "connected" });
+  client.on("message", (message) => handleMessage(message, client));
+});
 
-
-socket.on('connection',client =>{  
-    console.log("Connected", ' client id:', client.id);
-    client.emit('init',{message:'connected'})
-    client.on('message', message => handleMessage(message,client));
-})
-
-
-function handleMessage(message,client){
-    console.log(`${message.pavu}\r`);
-    if(message.pavu ==1){
-      try{
-        let x =  exec('pidof pavucontrol', function(err, out, code) {
-            if(out){
-                exec(`kill ${out}`,(err,out,code)=>{
-                    console.log("killed\r");
-                })
-            }
-            else{
-                exec(`pavucontrol`)
-            }
-       });
+async function cycleAudioOutput() { 
+ const { stdout, stderr } = await asyncExec(
+    "pacmd list-sinks | grep -e index:"
+  ).catch(error => Promise.reject('error while executing command'));
+  
+  if (stdout) {
+    let outSinkList = stdout
+      .trim()
+      .split("\n")
+      .map((outSink, index) => {
+        let splitSink = outSink.trim().split(":");
+        if (splitSink[0][0] === "*") currentSelectedSink = index;
+        return splitSink[1].trim();
+      });
+    
+      if(currentSelectedSink >= outSinkList.length-1){
+        currentSelectedSink = 0;
+      }else{
+        currentSelectedSink++;
       }
-      catch(e){
-        console.log(e);
-      }
-    }
-    // socket.sockets.emit('received',{message:message.pavu,sender:client.id,type:'received'})
+      exec(`pactl set-default-sink ${outSinkList[currentSelectedSink]}`);
+
+  }
 }
 
+function openPavucontrol() {
+  let x = exec("pidof pavucontrol", function (err, out, code) {
+    if (out) {
+      exec(`kill ${out}`, (err, out, code) => {
+        console.log("killed\r");
+      });
+    } else {
+      exec(`pavucontrol`);
+    }
+  });
+}
+
+function handleMessage(message, client) {
+  console.log(`${message.pavu}\r`);
+  if (message.pavu == 1) {
+    try {
+      cycleAudioOutput().catch(error=> console.log(error));
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
 
 socket.listen(3000);
