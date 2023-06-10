bae4519c2e97a4d3cca7aa9ee52f7b94f575db97
raushanraja <vraushanraja@gmail.com>
2023-06-10 18:12:32 +0530

    update socketio rotary code

--- wemos-d1-r2/SocketIO/socketiotry/include/wifisetup.h
+++ wemos-d1-r2/SocketIO/socketiotry/include/wifisetup.h
@@ -1,13 +1,15 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+
+const char *SSID = "SSID";
+const char *PASSWORD = "PASSWORD";
+
 void startWiFi()
 {
     WiFi.disconnect();
     Serial.println("Inside Start WiFi");
     delay(1000);
-    // WiFi.begin("TP-Link_9C82", "38702303");
-    // WiFi.begin("redmii", "1234467890");
-    WiFi.begin("dlink_DWR-920V_6124", "Dcvve45447");
+    WiFi.begin(SSID, PASSWORD);
     while (WiFi.status() != WL_CONNECTED)
     {
         delay(500);
--- wemos-d1-r2/SocketIO/socketiotry/src/main.cpp
+++ wemos-d1-r2/SocketIO/socketiotry/src/main.cpp
@@ -6,67 +6,94 @@
 #include <SocketIOclient.h>
 #include <Hash.h>
 
+// Rotary Encoder Inputs
+#define CLK D1
+#define DT D2
+#define SW D5
+
+// Rotary Encoder Vairalbes
+int counter = 0;
+int currentStateCLK;
+int lastStateCLK;
+String currentDir = "";
+unsigned long lastButtonPress = 0;
+int lastButtonState = 1;
 
 SocketIOclient socketIO;
 const int pavu = D5;
 const int discord = D6;
-int LED_STATE=HIGH;
-// #define USE_SERIAL Serial
+int LED_STATE = HIGH;
+#define USE_SERIAL Serial
 
-void socketIOEvent(socketIOmessageType_t type, uint8_t * payload, size_t length) {
-        switch(type) {
-        case sIOtype_DISCONNECT:
-            // USE_SERIAL.printf("[IOc] Disconnected!\n");
-            break;
-        case sIOtype_CONNECT:
-            // USE_SERIAL.printf("[IOc] Connected to url: %s\n", payload);
-            break;
-        case sIOtype_EVENT:
-            // USE_SERIAL.printf("[IOc] get event: %s\n", payload);
-            break;
-        case sIOtype_ACK:
-            // USE_SERIAL.printf("[IOc] get ack: %u\n", length);
-            // hexdump(payload, length);
-            break;
-        case sIOtype_ERROR:
-            // USE_SERIAL.printf("[IOc] get error: %u\n", length);
-            // hexdump(payload, length);
-            break;
-        case sIOtype_BINARY_EVENT:
-            // USE_SERIAL.printf("[IOc] get binary: %u\n", length);
-            // hexdump(payload, length);
-            break;
-        case sIOtype_BINARY_ACK:
-            // USE_SERIAL.printf("[IOc] get binary ack: %u\n", length);
-            // hexdump(payload, length);
-            break;
+void socketIOEvent(socketIOmessageType_t type, uint8_t *payload, size_t length)
+{
+    switch (type)
+    {
+    case sIOtype_DISCONNECT:
+        USE_SERIAL.printf("[IOc] Disconnected!\n");
+        break;
+    case sIOtype_CONNECT:
+        USE_SERIAL.printf("[IOc] Connected to url: %s\n", payload);
+
+        // join default namespace (no auto join in Socket.IO V3)
+        socketIO.send(sIOtype_CONNECT, "/");
+        break;
+    case sIOtype_EVENT:
+        USE_SERIAL.printf("[IOc] get event: %s\n", payload);
+        hexdump(payload, length);
+        break;
+    case sIOtype_ACK:
+        USE_SERIAL.printf("[IOc] get ack: %u\n", length);
+        hexdump(payload, length);
+        break;
+    case sIOtype_ERROR:
+        USE_SERIAL.printf("[IOc] get error: %u\n", length);
+        hexdump(payload, length);
+        break;
+    case sIOtype_BINARY_EVENT:
+        USE_SERIAL.printf("[IOc] get binary: %u\n", length);
+        hexdump(payload, length);
+        break;
+    case sIOtype_BINARY_ACK:
+        USE_SERIAL.printf("[IOc] get binary ack: %u\n", length);
+        hexdump(payload, length);
+        break;
     }
 }
 
-void setup() {
-    // // USE_SERIAL.begin(921600);
-
-    pinMode(pavu,INPUT_PULLUP);
-    pinMode(discord,INPUT_PULLUP);
+void setup()
+{
     // USE_SERIAL.begin(9600);
 
-    //Serial.setDebugOutput(true);
-    // USE_SERIAL.setDebugOutput(true);
+    // Set encoder pins as inputs
+    pinMode(CLK, INPUT);
+    pinMode(DT, INPUT);
+    pinMode(SW, INPUT_PULLUP);
+
+    USE_SERIAL.begin(9600);
+
+    Serial.setDebugOutput(true);
+    USE_SERIAL.setDebugOutput(true);
+
+    // Read the initial state of CLK
+    lastStateCLK = digitalRead(CLK);
+    lastButtonState = 1;
 
     // USE_SERIAL.println();
     // USE_SERIAL.println();
     // USE_SERIAL.println();
 
-      for(uint8_t t = 4; t > 0; t--) {
-          // USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\r", t);
-          // USE_SERIAL.flush();
-          delay(1000);
-      }
+    for (uint8_t t = 4; t > 0; t--)
+    {
+        USE_SERIAL.printf("[SETUP] BOOT WAIT %d...\r", t);
+        USE_SERIAL.flush();
+        delay(1000);
+    }
 
     startWiFi();
 
     // server address, port and URL
-    socketIO.begin("192.168.0.175", 3000);
+    socketIO.begin("192.168.68.55", 5000, "/socket.io/?EIO=4");
 
     // event handler
     socketIO.onEvent(socketIOEvent);
@@ -75,22 +102,74 @@ void setup() {
 unsigned long messageTimestamp = 0;
 int prev = 2;
 int prevDiscord = 2;
-void loop() {
+void loop()
+{
     socketIO.loop();
-    int bs = digitalRead(pavu);
-    int ds = digitalRead(discord);
-    if(bs != prev) prev = 2;
-    if(ds !=prevDiscord) prevDiscord = 2;
-    if(bs == LOW && prev != bs) {     
-        prev = bs;
-        socketIO.sendEVENT("[\"message\",{\"a\":1,\"d\":0}]");
-        delay(300);
+
+    currentStateCLK = digitalRead(CLK);
+
+    // If last and current state of CLK are different, then pulse occurred
+    // React to only 1 state change to avoid double count
+    if (currentStateCLK != lastStateCLK && currentStateCLK == 1)
+    {
+
+        // If the DT state is different than the CLK state then
+        // the encoder is rotating CCW so decrement
+        if (digitalRead(DT) != currentStateCLK)
+        {
+            counter--;
+            currentDir = "CCW";
+            String message = "[\"message\",{\"dir\":\"CCW\", \"counter\":" + String(counter) + "}]";
+            socketIO.sendEVENT(message);
+        }
+        else
+        {
+            // Encoder is rotating CW so increment
+            counter++;
+            currentDir = "CW";
+            String message = "[\"message\",{\"dir\":\"CW\", \"counter\":" + String(counter) + "}]";
+            socketIO.sendEVENT(message);
+        }
+
+        Serial.print("Direction: ");
+        Serial.print(currentDir);
+        Serial.print(" | Counter: ");
+        Serial.println(counter);
     }
 
-    if(ds == LOW &&  prevDiscord !=ds){
-        prevDiscord = ds;
-        socketIO.sendEVENT("[\"message\",{\"a\":0,\"d\":1}]");
-        delay(300);
+    // Remember last CLK state
+    lastStateCLK = currentStateCLK;
+
+    // Read the button state
+    int btnState = digitalRead(SW);
+
+    // If we detect LOW signal, button is pressed
+    if (btnState == LOW)
+    {
+        lastButtonState = 0;
+        // if 50ms have passed since last LOW pulse, it means that the
+        // button has been pressed, released and pressed again
+        if (millis() - lastButtonPress > 50)
+        {
+            Serial.println("Button pressed!");
+            String message = "[\"message\",{\"dir\":\"Button\", \"counter\": \"1\"}]";
+            socketIO.sendEVENT(message);
+        }
+
+        // Remember last button press event
+        lastButtonPress = millis();
     }
+
+    // Handle button relase
+    else
+    {
+        if (lastButtonState == 0)
+        {
+            Serial.println("Button released!");
+            lastButtonState = 1;
+            String message = "[\"message\",{\"dir\":\"Button\", \"counter\": \"0\"}]";
+            socketIO.sendEVENT(message);
+        }
+    }
+    delay(2);
 }
-
