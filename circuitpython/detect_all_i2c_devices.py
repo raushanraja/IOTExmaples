77489b68e4b3b73a24d00d825340763a66c72776
raushanraja <vraushanraja@gmail.com>
2023-12-29 12:14:42 +0530

    update comments

--- circuitpython/detect_all_i2c_devices.py
+++ circuitpython/detect_all_i2c_devices.py
@@ -2,12 +2,21 @@ import time
 import board
 import bitbangio
 
+# Define the I2C bus using bitbangio for ESP32-S3-Zero module
+# But same code can be used for other boards as well
+# Update the bus= bitbangio.I2C to select specific pins for SDA and SCL
 bus = bitbangio.I2C(board.D5, board.D6)
+
+# Unlock the I2C bus to enable configuration
 bus.unlock()
 
 while True:
     while not bus.try_lock():
         pass
-    print( "I2C devices found:", [hex(address) for address in bus.scan()],)
+    
+    # Scan for I2C devices and print their addresses
+    print("I2C devices found:", [hex(address) for address in bus.scan()])
+    
+    # Unlock the bus and wait for 0.5 seconds before scanning again
     bus.unlock()
     time.sleep(0.5)
